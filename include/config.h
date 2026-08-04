cf34d4592379dabb8bb597b59de88c46252bda0e
Avik43218 <roychoudhuryavik005@gmail.com>
2026-08-05 01:12:26 +0530

    refactor: Write config settings

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// CONFIG SETTINGS
+#define RS_ENC_PAYLOAD_SIZE 96
+#define PAYLOAD_SIZE 64
+#define PARITY_SIZE 32
+#define CHIP_RATE 16
+#define ALPHA 25.0
+
+#endif
+
