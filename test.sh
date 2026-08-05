58767328693a2456dc615a419a9900f0fa706f86
Avik43218 <roychoudhuryavik005@gmail.com>
2026-08-05 12:51:15 +0530

    feat: Add test.sh to streamline I/O

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo -e "\n========== BLUEGACHIIN ==========\n"
+
+if [[ "$1" == "-e" ]]; then
+
+	read -p "Enter target image path: " TARGET_IMG_PATH
+	read -p "Enter destination directory: " DEST_DIR
+	read -p "Enter payload: " PAYLOAD
+
+	cp "$TARGET_IMG_PATH" .
+	IMG_TITLE=$(basename "$TARGET_IMG_PATH")
+
+	make
+	./bin/bg --encode "$IMG_TITLE" "$PAYLOAD" "$BG_SECRET_KEY"
+
+	mv F_*.png "$DEST_DIR"
+	rm "$IMG_TITLE"
+
+elif [[ "$1" == "-d" ]]; then
+
+	read -p "Enter path of image to decode: " TARGET_IMG_PATH
+
+	cp "$TARGET_IMG_PATH" .
+	IMG_TITLE=$(basename "$TARGET_IMG_PATH")
+
+	make
+	./bin/bg --decode "$IMG_TITLE" "$BG_SECRET_KEY"
+
+	rm "$IMG_TITLE"
+
+fi
+
+
+
